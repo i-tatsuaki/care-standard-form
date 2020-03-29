82b2abef604c3954fb3d89fdeae913b11fb54672
tatsuaki <k05371@yahoo.co.jp>
2020-03-29 17:46:41 +0900

    feat: transit screen input file to service plan

--- src/main/kotlin/com/tatsuaki/carestandardform/controller/InputFileController.kt
+++ src/main/kotlin/com/tatsuaki/carestandardform/controller/InputFileController.kt
@@ -5,12 +5,12 @@ import com.tatsuaki.carestandardform.domain.serviceplanappendix.ServicePlanAppen
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class InputFileController() {
-    @GetMapping("servicePlan")
+    @PostMapping("servicePlan")
     fun servicePlan(model: Model): String {
-
         val servicePlan = ServicePlan()
         model.addAttribute("servicePlan", servicePlan)
         return "servicePlan"
@@ -18,9 +18,13 @@ class InputFileController() {
 
     @GetMapping("servicePlanAppendix")
     fun servicePlanAppendix(model: Model): String {
-
         val servicePlanAppendix = ServicePlanAppendix()
         model.addAttribute("servicePlanAppendix", servicePlanAppendix)
         return "servicePlanAppendix"
+    }
+
+    @GetMapping
+    fun inputFile(): String{
+        return "inputFile"
     }
 }
