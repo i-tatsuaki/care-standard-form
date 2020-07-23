a6473d7678b687b0166d6e8c47b5d2d05edbb399
tatsuaki <k05371@yahoo.co.jp>
2020-07-23 17:13:35 +0900

    feat: 利用者の選択ボタンにデザインを当てる

--- src/main/resources/static/script/servicePlan.js
+++ src/main/resources/static/script/servicePlan.js
@@ -1,12 +1,15 @@
 var selected = "0";
-
-let selector = document.getElementById("select-service-plan");
 let options = document.querySelectorAll("#select-service-plan option");
-selector.addEventListener("change", () => {
-    unShowTarget(selected);
-    selected = options[selector.selectedIndex].value
-    showTarget(selected);
-});
+
+Array.from(document.getElementsByClassName("user-select")).forEach(selectItem => {
+    selectItem.addEventListener("click",
+        (event) => {
+            unShowTarget(selected);
+            selected = event.target.dataset.value
+            showTarget(selected);
+        }
+    )
+})
 
 let unShowTarget = (index) => {
    let showTarget = document.getElementById("output-" + index);
